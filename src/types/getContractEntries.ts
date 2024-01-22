5f2c73c0ea16bd1681c39260b2a28facad2912f1
Matias Poblete <matias.pobleteduran@gmail.com>
2024-01-22 16:40:58 -0300

    Refactor factoryInstanceParser & add types

--- src/types/getContractEntries.ts
+++ src/types/getContractEntries.ts
@@ -11,5 +11,22 @@ export interface Edge {
 }
 
 export interface Node {
+  keyXdr: string;
   valueXdr: string;
+}
+
+export interface ParsedRouterEntry {
+  FeeTo: string;
+  FeeToSetter: string;
+  AllPairs: string[];
+  PairsMapping: PairMapping;
+  PairWasmHash: string;
+}
+
+export interface PairMapping {
+  pairs: Pair[];
+}  
+
+export interface Pair {
+  [pairsAdresses: string]: string;
 }
--- src/utils/parsers/factoryInstanceParser.ts
+++ src/utils/parsers/factoryInstanceParser.ts
@@ -1,14 +1,36 @@
-import { ContractEntriesResponse } from "../../types";
+import { ContractEntriesResponse, ParsedRouterEntry } from "../../types";
 import { scValToJs } from "../convert";
 import * as StellarSdk from "@stellar/stellar-sdk";
+enum DataKey {
+  FeeTo = 0,        // address public feeTo;
+  FeeToSetter = 1,  // address public feeToSetter;
+  AllPairs = 2,     //  address[] public allPairs;
+  PairsMapping = 3, // Map of pairs
+  PairWasmHash = 4,
+  FeesEnabled = 5, // bool is taking fees?
+}
 
-export const FactoryInstanceParser = (data: ContractEntriesResponse) => {
-  const base64Xdr = data.entryUpdateByContractId.edges[0].node.valueXdr
-  const parsedData:any = StellarSdk.xdr.ScVal.fromXDR(base64Xdr, "base64");
-  const jsValues: any = scValToJs(parsedData)
-  const storageValues = jsValues.storage()[2].val()
-  const parsedValues: Array<any> = scValToJs(storageValues)
-  console.log("values:", parsedValues)
-
-  return parsedValues;
+export const factoryInstanceParser = (data: ContractEntriesResponse) => {
+  if (!data.entryUpdateByContractId) {
+    throw new Error("No entries provided")
+  }
+  const parsedEntries: ParsedRouterEntry[] = []
+  for (const entry of data.entryUpdateByContractId.edges) {
+    const base64Xdr = entry.node.valueXdr
+    if (!base64Xdr) {
+        throw new Error("No valueXdr found in the entry")
+    }
+    const parsedData:any = StellarSdk.xdr.ScVal.fromXDR(base64Xdr, "base64");
+    const jsValues: any = scValToJs(parsedData)
+    const parsedValue = {} as ParsedRouterEntry
+    if(typeof(jsValues.storage) !== "undefined"){
+      for (let key in jsValues.storage()) {
+          const i: number = parseInt(key)
+          const element = jsValues.storage()[key].val()
+          Object.assign(parsedValue, {[DataKey[i]]: scValToJs(element)})
+      }
+    parsedEntries.push(parsedValue)
+    }
+  }
+  return parsedEntries;
 };
