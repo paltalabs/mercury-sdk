b653a16971fa38717bba8bd48e03701e856d9fa7
Matias Poblete <matias.pobleteduran@gmail.com>
2024-01-23 18:19:12 -0300

    Update contract address in dev.ts

--- src/dev.ts
+++ src/dev.ts
@@ -22,11 +22,11 @@ dotenv.config();
         password: process.env.MERCURY_TESTER_PASSWORD!,
     });
 
-    const routerContractAddress = "CDSUTAZNBTBAMG2SVZ63FRIBIJOEBSRVVR4GZ3TDXX25AHUN5N3ZYMYU";
+    const factoryAddress = "CDSUTAZNBTBAMG2SVZ63FRIBIJOEBSRVVR4GZ3TDXX25AHUN5N3ZYMYU";
     const pairAddress = "CDYLINP2CX64S2YC4CCI44XH4H7K6Z2WB5UV3U33VIK36T7YATR2QTXP"
     const nullAddress= "CAFQFTDI3TW4BIK3UCDWV5VWODDYIOSCBZPS3LUHXE5PAPFCJMXM4QRJ"
     const args = {
-        contractId: routerContractAddress,
+        contractId: factoryAddress,
         keyXdr: "AAAFA==",
         durability: "persistent"
     }
@@ -34,13 +34,13 @@ dotenv.config();
         console.error(err)
     })
     console.log(subscribe) */
-    const entries: ApiResponse<any> | void = await mercuryInstance.getContractEntries(args)
+    const factoryEntries: ApiResponse<any> | void = await mercuryInstance.getContractEntries(args)
     .catch((err: any) => {
         console.log(err)
     })
-    if(entries && entries.ok){
-        const parsedEntries: ParsedRouterEntry[] = factoryInstanceParser(entries.data!)
-        console.log(parsedEntries[1].AllPairs)
+    if(factoryEntries && factoryEntries.ok){
+        const parsedEntries: ParsedRouterEntry[] = factoryInstanceParser(factoryEntries.data)
+        console.log(parsedEntries[0].AllPairs)
     }
     const pairContractArgs= {
         contractId: pairAddress,
